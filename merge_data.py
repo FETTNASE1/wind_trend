72f81c9171784763935f95291a27842c3c17c98f
Zunka42 <soenkeherrmann@gmx.de>
2024-04-09 18:09:12 +0200

    this merges all years to one dataset

---
+++ merge_data.py
@@ -0,0 +1,11 @@
+# guided by https://tutorial.xarray.dev/fundamentals/03.2_groupby_with_xarray.html
+import xarray as xr
+
+# load ncdf file to xarray
+ds = xr.open_mfdataset("*_wind_helgoland.nc")# requires `pip install dask`
+
+from numpy import sqrt
+# add absolute wind speed
+ds["windspeed"] = sqrt(ds.u10*ds.u10 + ds.v10*ds.v10)
+
+xr.save_mfdataset([ds], ["helgoland_wind.nc"], mode='a')
